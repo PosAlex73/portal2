06d733e13ddd06c61e28f20a6b50530b8115e053
PosAlex73 <alternativa988@inbox.ru>
2022-04-10 13:28:09 +0400

    add public files

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    public function dashboard()
+    {
+        return view('admin.views.dashboard');
+    }
 }
