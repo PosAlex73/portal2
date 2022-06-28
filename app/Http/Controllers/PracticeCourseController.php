59e1ff4a1a2928cf254d5ac2e2d17cf1eadf71a2
AlexPos73 <alternativa988@inbox.ru>
2022-06-28 23:54:21 +0400

    fix orchid classes

---
+++ app/Http/Controllers/PracticeCourseController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePracticeCourseRequest;
+use App\Http\Requests\UpdatePracticeCourseRequest;
+use App\Models\PracticeCourse;
+
+class PracticeCourseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePracticeCourseRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePracticeCourseRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PracticeCourse  $practiceCourse
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PracticeCourse $practiceCourse)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PracticeCourse  $practiceCourse
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PracticeCourse $practiceCourse)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePracticeCourseRequest  $request
+     * @param  \App\Models\PracticeCourse  $practiceCourse
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePracticeCourseRequest $request, PracticeCourse $practiceCourse)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PracticeCourse  $practiceCourse
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PracticeCourse $practiceCourse)
+    {
+        //
+    }
+}
