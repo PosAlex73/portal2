cb3aa27704f3d7061e3adf594cf07077530a1839
AlexPos73 <alternativa988@inbox.ru>
2022-07-30 23:47:30 +0400

    add ace editor for tasks

--- resources/views/front/user_progress/task_template/practice.blade.php
+++ resources/views/front/user_progress/task_template/practice.blade.php
@@ -1,3 +1,21 @@
+<style type="text/css" media="screen">
+    #monaco-editor {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+    }
+</style>
+
 <div id="monaco-editor">
 
 </div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function(){
+        const editor = ace.edit("monaco-editor");
+        editor.setTheme("ace/theme/dracula");
+        editor.session.setMode("ace/mode/php");
+    });
+</script>
