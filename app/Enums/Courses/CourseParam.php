096cb9604c4c3413465452ed3a0b33412f4db976
aposnov <a.posnov@fairtech.ru>
2023-05-23 23:05:42 +0400

    add course params

---
+++ app/Enums/Courses/CourseParam.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums\Courses;
+
+use App\Enums\Enumable;
+
+class CourseParam
+{
+    use Enumable;
+
+    public const SUBSCRIBE = 'S';
+}
---
+++ database/migrations/2023_05_21_053143_create_course_params_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_params', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('course_id');
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->string('subscribes', 1)->default(0)->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_params');
+    }
+};
