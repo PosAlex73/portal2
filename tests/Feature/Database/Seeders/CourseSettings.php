d1d24ce8faf865d45d167b596e61303eec3b386f
PosAlex73 <alternativa988@inbox.ru>
2023-07-10 23:11:35 +0400

    refactoting model fields

---
+++ tests/Feature/Database/Seeders/CourseSettings.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature\Database\Seeders;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CourseSettings extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
